c4339d51eb4f912637a2250db7fb3b1d1e43cac5
polsmeister <71605964+polsmeister@users.noreply.github.com>
2021-02-16 14:38:13 +0200

    jees

--- signincheck.php
+++ signincheck.php
@@ -4,7 +4,7 @@ $json=$_POST["elina19161_tunnukset"];
 $tunnukset=json_decode($json, false);
 
 $username = $tunnukset->username;
-$password= $tunnukset->password;
+$password = $tunnukset->password;
 
 if (empty($username) || empty($password)) {
 	print "Täytä kaikki tiedot.";
@@ -15,7 +15,7 @@ if (empty($username) || empty($password)) {
 $yhteys = mysqli_connect("localhost", "trtkp20a3","trtkp20a3passwd");
 
 #virheilmoitukset, jos tietokantaan ei saada yhteyttä
-if (!yhteys){
+if (!$yhteys){
 	print "Tietojen tallennuksessa tapahtui virhe.";
 	exit;
 }
@@ -28,9 +28,9 @@ if  (!$tietokanta) {
 }
 
 #jos tietokantaan saadaan yhteys, tallennetaan tiedot valittuun tietokantaan.
-$sql='insert into elina19161_tunnukset(username, password) values (?,?)";
+$sql="insert into elina19161_tunnukset(username, password) values (?,?)";
 $stmt=mysqli_prepare($yhteys, $sql);
-mysqli_stmt_bind_param($stmt, 'ss', $username, $password);
+mysqli_stmt_bind_param($stmt, "ss", $username, $password);
 mysqli_stmt_execute($stmt);
 mysqli_stmt_close($yhteys);
 mysqli_close($yhteys);
