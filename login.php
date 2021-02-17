f292fa29713d3bc7f709031981863f3b01155276
polsmeister <71605964+polsmeister@users.noreply.github.com>
2021-02-17 15:35:24 +0200

    jees

--- login.php
+++ login.php
@@ -5,26 +5,31 @@ $username =$_POST["username"];
 $password =$_POST["password"];
 
 $yhteys = mysqli_connect("localhost", "trtkp20a3", "trtkp20a3passwd");
-
+$tietokanta=mysqli_select_db($yhteys, "trtkp20a3");
 if (!$yhteys){
     header("Location:error.html");
     exit;
 }
 
-$ok=mysqli_select_db($yhteys, "trtkp20a3");
 
-if (!$ok){
+if (!$tietokanta){
     header("Location:error.html");
     exit;
 } 
 
-$tulos=mysqli_query($yhteys, "select * from user where tunnus='$username' and salasana=' ".md5($password). "'");
+$sql="select * from elina19161_tunnukset where tunnus='".$username."' and salasana='".md5($password)."'";
+$stmt=mysqli_prepare($yhteys, $sql);
+mysqli_stmt_bind_param($stmt, "ss", $username, $password);
+mysqli_stmt_execute($stmt);
+$tulos=mysqli_stmt_get_result($stmt);
 
-if ($tulos){
-    $_SESSION["user"]="OK";
-    header("Location:profile.html");
+
+if ($rivi=mysqli_fetch_object($tulos)){
+    $_SESSION["user_ok"]="ok";
+    header("Location:".$_SESSION["paluuosoite"]);
     exit;
 }
-header("Location:login.html");
-
+else{
+	header("Location:login.html");
+}
 ?>
