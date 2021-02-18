9c32d1ed5335f242054fbd25ad7a4f813fb281cb
BookBunny <75021307+BookBunny@users.noreply.github.com>
2021-02-18 10:30:26 +0200

    nonii toimisko nyt part miljoona

--- login.php
+++ login.php
@@ -3,28 +3,33 @@ session_start();
 
 $username =$_POST["username"];
 $password =$_POST["password"];
-
+print "$username $password";
 $yhteys = mysqli_connect("localhost", "trtkp20a3", "trtkp20a3passwd");
 
 if (!$yhteys){
     header("Location:error.html");
     exit;
 }
+$tietokanta=mysqli_select_db($yhteys, "trtkp20a3");
 
-$ok=mysqli_select_db($yhteys, "trtkp20a3");
-
-if (!$ok){
+if (!$tietokanta){
     header("Location:error.html");
     exit;
 } 
 
-$tulos=mysqli_query($yhteys, "select * from user where tunnus='$username' and salasana=' ".md5($password). "'");
+$sql="select * from elina19161_tunnukset where username=? and password=md5(?)";
+$stmt=mysqli_prepare($yhteys, $sql);
+mysqli_stmt_bind_param($stmt, "ss", $username, $password);
+mysqli_stmt_execute($stmt);
+$tulos=mysqli_stmt_get_result($stmt);
+print $_SESSION["paluuosoite"];
 
-if ($tulos){
-    $_SESSION["user"]="OK";
-    header("Location:profile.html");
+if ($rivi=mysqli_fetch_object($tulos)){
+    $_SESSION["user_ok"]="ok";
+    header("Location:".$_SESSION["paluuosoite"]);
     exit;
 }
-header("Location:login.html");
-
-?>
+else{
+	header("Location:login.html");
+} 
+?> 
